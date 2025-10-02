b0004b43ce72ccd2c38092687ee3499407fd729e
Yuechun Qin <Yqin2-cosc381@users.noreply.github.com>
2025-10-02 00:16:02 +0000

    unit test changes

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -1,7 +1,6 @@
 import pytest
-from presidio_anonymizer.entities import  EngineResult, OperatorResult
 from presidio_anonymizer.sample import  sample_run_anonymizer
-
+import json
 
 @pytest.mark.parametrize(
     "text, start, end,expected_result",
@@ -9,17 +8,14 @@ from presidio_anonymizer.sample import  sample_run_anonymizer
         ("My name is Bond.",11,15,
         {
             "text": "My name is BIP.",
-            "items":  {'start': 11, 'end': 14, 'entity_type': 'PERSON', 'text': 'BIP', 'operator': 'replace'}
+            "items":  [
+                {'start': 11, 'end': 14, 'entity_type': 'PERSON', 'text': 'BIP', 'operator': 'replace'}
+            ]
         })
     ],
 )
 def test_sample_run_anonymizer(text,start,end,expected_result):
     result = sample_run_anonymizer(text,start,end)
-    expected_engine_result = create_engine_result(expected_result)
-    assert expected_engine_result == result
-
-def create_engine_result(json):
-    operation_result:OperatorResult = OperatorResult.from_json(json["items"])
-    result = EngineResult(text=json["text"],items = [operation_result])
-    return result
-
+    result_json = result.to_json()
+    expected_json= json.dumps(expected_result)
+    assert result_json == expected_json
